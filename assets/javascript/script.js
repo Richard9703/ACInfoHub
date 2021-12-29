52db76783b8d430696f2645300d649bfeede22fb
Richard9703 <richardpaulov@gmail.com>
2021-12-29 17:51:00 +0000

    test api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,49 +1,49 @@
-const app = document.getElementById('root')
+const app = document.getElementById('root');
 
-const logo = document.createElement('img')
-logo.src = 'https://static.wikia.nocookie.net/animalcrossing/images/9/92/NewHorizons.png/revision/latest?cb=20210123051957'
+const logo = document.createElement('img');
+logo.src = 'https://static.wikia.nocookie.net/animalcrossing/images/9/92/NewHorizons.png/revision/latest?cb=20210123051957';
 
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
 
-app.appendChild(logo)
-app.appendChild(container)
+app.appendChild(logo);
+app.appendChild(container);
 
 // Create a requests var and give new XMLHttpRequest object to it.
-var request = new XMLHttpRequest ()
+var request = new XMLHttpRequest ();
 
 // Open connection to API (ACNHApi) using GET erquest on URL endpoint.
-request.open('GET', 'https://acnhapi.com/v1a/fish/{fishID}', true)
+request.open('GET', ' https://ghibliapi.herokuapp.com/films', true);
 request.onload = function () {
     // Access JSON data here
-   var data = JSON.parse(this.response)
+   var data = JSON.parse(this.response);
 
    if (request.status >= 200 && request.status < 400) {
-    data.forEach(fish => {
-       const card = document.createElement('div')
-       card.setAttribute('class', 'card')
+    data.forEach(movie => {
+       const card = document.createElement('div');
+       card.setAttribute('class', 'card');
 
-       const h1 = document.createElement('h1')
-       h1.textContent = fish.price
+       const h1 = document.createElement('h1');
+       h1.textContent = movie.title;
 
-       const p = document.createElement('p')
-       fish.description = fish.availability.substring(0, 300)
-       p.textContent  = `${fish.availability}...`
+       const p = document.createElement('p');
+       movie.description = movie.description.substring(0, 300);
+       p.textContent  = `${movie.description}...`;
 
-       container.appendChild(card)
+       container.appendChild(card);
 
-       card.appendChild(h1)
-       card.appendChild(p)
-    })
+       card.appendChild(h1);
+       card.appendChild(p);
+    });
    }  else {
-       const errorMessage = document.createElement('marquee')
-       errorMessage.textContent = `Gah, it's not working!`
-       app.appendChild(errorMessage)
+       const errorMessage = document.createElement('marquee');
+       errorMessage.textContent = `Gah, it's not working!`;
+       app.appendChild(errorMessage);
    }
-}
+};
 
 // Send request
-request.send()
+request.send();
 
 
 
