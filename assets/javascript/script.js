a0f0bdac9fea6292e1b36457fc7211a628ead25e
Richard9703 <richardpaulov@gmail.com>
2021-12-30 17:21:12 +0000

    search bar

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,57 +1,59 @@
-const app = document.getElementById('root');
+// const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = '';
+// const logo = document.createElement('img');
+// logo.src = '';
 
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
+// const container = document.createElement('div');
+// container.setAttribute('class', 'container');
 
-app.appendChild(logo);
-app.appendChild(container);
+// app.appendChild(logo);
+// app.appendChild(container);
 
-// Create a requests var and give new XMLHttpRequest object to it.
-var request = new XMLHttpRequest ();
+// // Create a requests var and give new XMLHttpRequest object to it.
+// var request = new XMLHttpRequest ();
 
-// Open connection to API (ACNHApi) using GET erquest on URL endpoint.
-request.open('GET', 'https://acnhapi.com/v1a/fish/', true);
-request.onload = function () {
-    // Access JSON data here
-   var data = JSON.parse(this.response);
+// // Open connection to API (ACNHApi) using GET erquest on URL endpoint.
+// request.open('GET', 'https://acnhapi.com/v1a/fish/', true);
+// request.onload = function () {
+//     // Access JSON data here
+//    var data = JSON.parse(this.response);
 
-   if (request.status >= 200 && request.status < 400) {
-    data.forEach(fish => {
+//    if (request.status >= 200 && request.status < 400) {
+//     data.forEach(fish => {
        
 
-       // Card  
-       const card = document.createElement('div');
-       card.setAttribute('class', 'card');
+//        // Card  
+//        const card = document.createElement('div');
+//        card.setAttribute('class', 'card');
 
-       // Fish name
-       const h1 = document.createElement('h1');
-       h1.textContent = fish.name["name-EUen"];
+//        // Fish name
+//        const h1 = document.createElement('h1');
+//        h1.textContent = fish.name["name-EUen"];
        
-       // Information
-       const p = document.createElement('p');
-       fish['catch-phrase'] =  fish['catch-phrase'].substring(0, 300);
-       p.textContent  = `${ fish['catch-phrase']}...`;
+//        // Information
+//        const p = document.createElement('p');
+//        fish['catch-phrase'] =  fish['catch-phrase'].substring(0, 300);
+//        p.textContent  = `${ fish['catch-phrase']}...`;
 
 
-        container.appendChild(card);
+//         container.appendChild(card);
 
-        card.appendChild(h1);
-        card.appendChild(p);
+//         card.appendChild(h1);
+//         card.appendChild(p);
     
-    });
-   }  else {
-       const errorMessage = document.createElement('marquee');
-       errorMessage.textContent = `Gah, it's not working!`;
-       app.appendChild(errorMessage);
-   }
-};
+//     });
+//    }  else {
+//        const errorMessage = document.createElement('marquee');
+//        errorMessage.textContent = `Error!`;
+//        app.appendChild(errorMessage);
+//    }
+// };
 
-// Send request
-request.send();
+// // Send request
+// request.send();
 
-
+function searchFunction() {
+    var input, filter,
+}
 
 
